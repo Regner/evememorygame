070a177db147373da64f07422ee9dbcae2433b62
Regner Blok-Andersen <shadowdf@gmail.com>
2015-03-10 15:28:01 +0000

    add sub category links

--- evememorygame/modules/questions/api.py
+++ evememorygame/modules/questions/api.py
@@ -13,9 +13,42 @@ class QuestionsCollection(Resource):
     def get(self):
         questions = {
             'categories' : {
-                'random'   : {'href' : url_for('question_random_category',   _external=True)},
-                'universe' : {'href' : url_for('question_universe_category', _external=True)},
-                'ships'    : {'href' : url_for('question_ships_category',    _external=True)},
+                'random' : {
+                    'href' : url_for('question_random_category',   _external=True),
+                    'name' : 'Random',
+                },
+                'universe' : {
+                    'href'           : url_for('question_universe_category', _external=True),
+                    'name'           : 'Universe',
+                    'sub_categories' : {
+                        'bordering_region' : {
+                            'href' : url_for('question_universe_bordering_region_category', _external=True),
+                            'name' : 'Bordering Regions',
+                        },
+                        'poitot' : {
+                            'href' : url_for('question_universe_poitot_category', _external=True),
+                            'name' : 'Poitot',
+                        },
+                    },
+                },
+                'ships' : {
+                    'href' : url_for('question_ships_category',    _external=True),
+                    'name' : 'Ships',
+                    'sub_categories' : {
+                        'class_id' : {
+                            'href' : url_for('question_ships_class_id_category', _external=True),
+                            'name' : 'Class Identification',
+                        },
+                        'image_id' : {
+                            'href' : url_for('question_ships_image_id_category', _external=True),
+                            'name' : 'Image Identification',
+                        },
+                        'slots' : {
+                            'href' : url_for('question_ships_slots_category', _external=True),
+                            'name' : 'Slots',
+                        },
+                    },
+                },
             }
         }
         
@@ -48,6 +81,30 @@ class QuestionUniverseCategory(Resource):
         return result
 
 
+class QuestionUniverseBorderingRegionCategory(Resource):
+    """ Resource for getting a question about what region borders a given region. """
+    
+    def get(self):
+        universe = QuestionUniverse()
+        
+        result = universe.region_boardering_region()
+        result['category'] = universe.__class__.__name__.lower()
+        
+        return result
+
+
+class QuestionUniversePoitotCategory(Resource):
+    """ Resource for getting a question about Poitot. """
+    
+    def get(self):
+        universe = QuestionUniverse()
+        
+        result = universe.region_with_poitot()
+        result['category'] = universe.__class__.__name__.lower()
+        
+        return result
+
+
 class QuestionShipsCategory(Resource):
     """ Resource for getting a question from the universe category. """
     
@@ -61,20 +118,59 @@ class QuestionShipsCategory(Resource):
         return result
 
 
+class QuestionShipsClassCategory(Resource):
+    """ Resource for getting a question about what class a ship is. """
+    
+    def get(self):
+        ships = QuestionShip()
+        
+        result = ships.ship_class()
+        result['category'] = ships.__class__.__name__.lower()
+        
+        return result
+
+
+class QuestionShipsIdentificationCategory(Resource):
+    """ Resource for getting a question about identifying a ship from an image. """
+    
+    def get(self):
+        ships = QuestionShip()
+        
+        result = ships.select_ship_from_image()
+        result['category'] = ships.__class__.__name__.lower()
+        
+        return result
+
+
+class QuestionShipsSlotsCategory(Resource):
+    """ Resource for getting a question about the number of slots a ship has. """
+    
+    def get(self):
+        ships = QuestionShip()
+        
+        result = ships.number_of_slots()
+        result['category'] = ships.__class__.__name__.lower()
+        
+        return result
+
+
 class Test(Resource):
     """ TESTING """
     
     def get(self):
-        test = get_dogma_attribute_for_type(641, 12)
-        print test
         return {}
     
 
 def register_apis(api):
     """ Registers the API resources defined in this file with the app in it's application factory. """
     
-    api.add_resource(Test,                     '/test/')
-    api.add_resource(QuestionsCollection,      '/questions/')
-    api.add_resource(QuestionRandomCategory,   '/questions/random/',   endpoint='question_random_category')
-    api.add_resource(QuestionUniverseCategory, '/questions/universe/', endpoint='question_universe_category')
-    api.add_resource(QuestionShipsCategory,    '/questions/ships/',    endpoint='question_ships_category')
+    api.add_resource(Test,                                    '/test/')
+    api.add_resource(QuestionsCollection,                     '/questions/')
+    api.add_resource(QuestionRandomCategory,                  '/questions/random/',                    endpoint='question_random_category')
+    api.add_resource(QuestionUniverseCategory,                '/questions/universe/',                  endpoint='question_universe_category')
+    api.add_resource(QuestionUniverseBorderingRegionCategory, '/questions/universe/bordering_region/', endpoint='question_universe_bordering_region_category')
+    api.add_resource(QuestionUniversePoitotCategory,          '/questions/universe/poitot/',           endpoint='question_universe_poitot_category')
+    api.add_resource(QuestionShipsCategory,                   '/questions/ships/',                     endpoint='question_ships_category')
+    api.add_resource(QuestionShipsClassCategory,              '/questions/ships/class_id/',            endpoint='question_ships_class_id_category')
+    api.add_resource(QuestionShipsIdentificationCategory,     '/questions/ships/image_id/',            endpoint='question_ships_image_id_category')
+    api.add_resource(QuestionShipsSlotsCategory,              '/questions/ships/slots/',               endpoint='question_ships_slots_category')
